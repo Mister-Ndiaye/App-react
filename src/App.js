937cc4dbf1ffe256458f40e0295040aeb30fe8f8
Mister-Ndiaye <albathie97@gmail.com>
2021-10-22 10:57:50 +0000

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+import axios from 'axios'
+import React from 'react'
 function App() {
+
+
+ // var paydunya = require('paydunya')
+  //console.log(paydunya)
+  const handleClick =()=>{    
+    
+    axios.get('https://springapiucad.herokuapp.com/paydunya').then(resp=>{
+      alert(resp.data); 
+    }).catch(err=>{
+      alert(err) ;
+    });
+
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello </h1>
+      <button onClick={handleClick} >Hello Word</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
